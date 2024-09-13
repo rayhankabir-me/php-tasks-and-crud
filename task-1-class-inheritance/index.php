65c459feadd2c4e6a751b68666d423b1f0672e4f
rayhankabir-me <devrayhankabir@gmail.com>
2024-09-13 20:06:12 +0600

    task 1 - class inheritance

---
+++ task-1-class-inheritance/index.php
@@ -0,0 +1,40 @@
+<?php
+abstract class Shape {
+    abstract public function area_calculation();
+}
+
+class Circle extends Shape {
+    private $radius;
+
+    public function __construct($radius)
+    {
+        $this->radius = $radius;
+    }
+
+    public function area_calculation()
+    {
+        return pi() * pow($this->radius, 2);
+    }
+}
+
+class Rectangle extends Shape {
+    private $width;
+    private $height;
+
+    public function __construct($width, $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function area_calculation()
+    {
+        return $this->width * $this->height;
+    }
+}
+
+$circle = new Circle(10);
+echo "area of circle is: ". $circle->area_calculation();
+echo "<br>";
+$rectangle = new Rectangle(5, 10);
+echo "area of rectangle is: ".$rectangle->area_calculation();
